58beb1391c2d86bc774778a376a10bd2d9295356
FRANCISCO CLÉCIO LIMA DA SILVA <FRANCISCO0794@DIGITALCOLLEGE.LOCAL>
2024-07-04 09:12:45 -0300

    treino de recursivo

---
+++ recursivo.js
@@ -0,0 +1,33 @@
+// var conteudo = 'x'
+// funcaoQueimprimialgo(conteudo)
+
+
+
+// function queSoma10Eimprimi(valor){
+//     console.log(valor+10);
+// }
+
+
+// function funcaoQueimprimialgo(conteud){
+//     console.log(conteud);
+//     queNaoFazNada()
+//     queSoma10Eimprimi(30)
+// }
+
+// function queNaoFazNada(){
+//     queSoma10Eimprimi(-1)
+// }
+
+// queNaoFazNada()
+
+// function queNaoFazNada(){
+//     console.log(1);
+//     queNaoFazNada()
+// }
+
+contagem(1)
+function contagem(valor){
+    console.log(valor);
+    valor += 1
+    contagem(valor)
+}
